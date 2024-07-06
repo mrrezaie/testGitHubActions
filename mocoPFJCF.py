208c9cb18859b8e8253f9130f32c17bd70de69e1
Mohammadreza Rezaie <mrrezaie@outlook.com>
2024-07-06 15:55:52 +0330

    test adaptive mu_strategy

--- mocoPFJCF.py
+++ mocoPFJCF.py
@@ -329,7 +329,9 @@ solver.set_optim_max_iterations(10000)
 # # implicit for inverse; explicit for forward dynamics
 # solver.set_multibody_dynamics_mode('explicit') 
 # solver.set_optim_finite_difference_scheme('central')
-solver.set_optim_hessian_approximation('exact')
+# solver.set_optim_hessian_approximation('exact')
+# solver.set_optim_nlp_scaling_method('gradient-based')
+solver.set_optim_mu_strategy('adaptive')
 # solver.set_parallel(0)
 
 
