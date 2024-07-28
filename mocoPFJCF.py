cec1fa980eb5dbe5eda97da809660d15a1c05064
Mohammadreza Rezaie <mrrezaie@outlook.com>
2024-07-28 16:36:44 +0330

    Update mocoPFJCF.py
    [skip ci]

--- mocoPFJCF.py
+++ mocoPFJCF.py
@@ -164,7 +164,7 @@ track = osim.MocoTrack()
 track.setModel( osim.ModelProcessor(model))
 track.set_initial_time(t0)
 track.set_final_time(t1)
-track.set_mesh_interval(0.02) # 100 Hz
+track.set_mesh_interval(0.02) # ~100 Hz (Hermite-Simpson)
 track.set_minimize_control_effort(True)
 track.set_control_effort_weight(controlW)
 
