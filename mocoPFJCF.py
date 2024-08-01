f9bb130fe07d13aadfdea532cf469ea57915f237
Mohammadreza Rezaie <mrrezaie@outlook.com>
2024-08-01 10:30:00 +0330

    exclude control effort

--- mocoPFJCF.py
+++ mocoPFJCF.py
@@ -164,8 +164,8 @@ track = osim.MocoTrack()
 track.setModel( osim.ModelProcessor(model))
 track.set_initial_time(t0)
 track.set_final_time(t1)
-track.set_mesh_interval(0.02) # ~100 Hz (Hermite-Simpson)
-track.set_minimize_control_effort(True)
+track.set_mesh_interval(0.05) # Hermite-Simpson
+track.set_minimize_control_effort(False)
 track.set_control_effort_weight(controlW)
 
 
