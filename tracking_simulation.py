4322e9e04872cad51fdd0952030386b2f1417355
Mohammadreza Rezaie <mrrezaie@outlook.com>
2024-08-09 14:24:08 +0330

    muscle, marker+grf, 0.1w marker, 1w grf, 0.001w control

--- tracking_simulation.py
+++ tracking_simulation.py
@@ -13,8 +13,8 @@ contact_tracking    = True
 joint_reaction_goal = False
 
 # goals weight
-markerW  = 1
-GRFW     = 10
+markerW  = 0.1
+GRFW     = 1
 controlW = 0.001 # (default==0.001 in MocoTrack)
 PFJLW    = 0.1
 
