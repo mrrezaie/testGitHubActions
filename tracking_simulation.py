1e28737669b9fbd976aa9f27fb67d7b38d392c9c
mrrezaie <mrrezaie@outlook.com>
2024-08-11 22:21:54 +0330

    scale_variables_using_bounds

--- tracking_simulation.py
+++ tracking_simulation.py
@@ -334,12 +334,13 @@ solver.set_optim_max_iterations(10000)
 # solver.set_implicit_multibody_accelerations_weight(1)
 # solver.set_minimize_implicit_auxiliary_derivatives(True)
 # solver.set_implicit_auxiliary_derivatives_weight(1)
-solver.set_multibody_dynamics_mode('explicit') # explicit
+# solver.set_multibody_dynamics_mode('explicit') # explicit
 # solver.set_transcription_scheme('hermite-simpson') # trapezoidal
 # solver.set_interpolate_control_midpoints(True)
 # solver.set_enforce_path_constraint_midpoints(True)
 # solver.set_enforce_constraint_derivatives(True)
 solver.set_optim_finite_difference_scheme('backward') # central forward backward
+solver.set_scale_variables_using_bounds(True)
 # solver.set_optim_sparsity_detection() # none random initial-guess
 # solver.set_optim_hessian_approximation('exact') # exact limited-memory
 # # solver.set_optim_nlp_scaling_method('gradient-based')
