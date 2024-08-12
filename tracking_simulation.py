2a33597801a5b38be5e7542075642f8eda20efb3
mrrezaie <mrrezaie@outlook.com>
2024-08-12 17:19:07 +0330

    0.05w grf, 0.0001w c

--- tracking_simulation.py
+++ tracking_simulation.py
@@ -14,7 +14,7 @@ joint_reaction_goal = False
 
 # goals weight
 marker_weight  = 1
-grf_weight     = 0.001
+grf_weight     = 0.05
 control_weight = 0.0001 # (default==0.001 in MocoTrack)
 # PFJL_weight    = 0.1
 
