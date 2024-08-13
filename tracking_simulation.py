1edba7b008079858cfb41ff20075f78764a5628d
mrrezaie <mrrezaie@outlook.com>
2024-08-13 17:27:26 +0330

    free mtp

--- tracking_simulation.py
+++ tracking_simulation.py
@@ -78,8 +78,9 @@ model = osim.Model(model_path)
 for cName in ['mtp_angle_r', 'mtp_angle_l']:
     coordinate = model.getCoordinateSet().get(cName)
     coordinate.set_range(0, -80*osim.SimTK_DEGREE_TO_RADIAN) # adjust the min range
-osim.ModelFactory().replaceJointWithWeldJoint(model, 'mtp_r')
-osim.ModelFactory().replaceJointWithWeldJoint(model, 'mtp_l')
+# # convert mtp joints to weld
+# osim.ModelFactory().replaceJointWithWeldJoint(model, 'mtp_r')
+# osim.ModelFactory().replaceJointWithWeldJoint(model, 'mtp_l')
 
 # adjust patellofemoral joint range of motion
 for cName in ['knee_angle_r_beta', 'knee_angle_l_beta']:
