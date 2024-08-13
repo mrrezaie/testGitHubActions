52c8055395b01a8888c64320dae235248aee5a6d
mrrezaie <mrrezaie@outlook.com>
2024-08-13 04:29:16 +0330

    7 shperes

--- tracking_simulation.py
+++ tracking_simulation.py
@@ -165,13 +165,13 @@ if contact_tracking:
     calcn = model.getBodySet().get(f'calcn_{s}')
     toes  = model.getBodySet().get(f'toes_{s}')
     spheres = {
-        's1': osim.ContactSphere(0.020, osim.Vec3([0.010,+0.000,-0.005]), calcn, f's1_{s}'),
-        's2': osim.ContactSphere(0.020, osim.Vec3([0.070,-0.003,-0.025]), calcn, f's2_{s}'),
-        # 's3': osim.ContactSphere(0.020, osim.Vec3([0.070,-0.003,+0.022]), calcn, f's3_{s}'),
-        's4': osim.ContactSphere(0.020, osim.Vec3([0.165,-0.003,-0.027]), calcn, f's4_{s}'),
-        's5': osim.ContactSphere(0.020, osim.Vec3([0.125,-0.003,+0.035]), calcn, f's5_{s}'),
-        's6': osim.ContactSphere(0.020, osim.Vec3([0.030,-0.003,-0.000]), toes,  f's6_{s}'),
-        's7': osim.ContactSphere(0.020, osim.Vec3([0.000,-0.000,+0.030]), toes,  f's7_{s}'),
+        's1': osim.ContactSphere(0.025, osim.Vec3([0.010,+0.002,-0.005]), calcn, f's1_{s}'),
+        's2': osim.ContactSphere(0.025, osim.Vec3([0.085,-0.002,-0.025]), calcn, f's2_{s}'),
+        's3': osim.ContactSphere(0.025, osim.Vec3([0.070,-0.002,+0.022]), calcn, f's3_{s}'),
+        's4': osim.ContactSphere(0.025, osim.Vec3([0.165,-0.002,-0.027]), calcn, f's4_{s}'),
+        's5': osim.ContactSphere(0.025, osim.Vec3([0.125,-0.002,+0.035]), calcn, f's5_{s}'),
+        's6': osim.ContactSphere(0.025, osim.Vec3([0.030,-0.002,-0.000]), toes,  f's6_{s}'),
+        's7': osim.ContactSphere(0.025, osim.Vec3([0.000,-0.002,+0.030]), toes,  f's7_{s}'),
         }
     floor = osim.ContactHalfSpace(osim.Vec3([0.500,-0.000,-0.250]), 
                                   osim.Vec3([0,0,-osim.SimTK_PI/2]), ground, 'floor')
