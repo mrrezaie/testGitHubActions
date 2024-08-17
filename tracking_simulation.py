1fff50796150a09865d2c7ecd5606a7185d797c9
mrrezaie <mrrezaie@outlook.com>
2024-08-17 11:29:20 +0330

    1e-4 tols

--- tracking_simulation.py
+++ tracking_simulation.py
@@ -30,8 +30,8 @@ else:
     residuals_weight = 100 # increase the their weights in control-effort goal
 
 # solver tolerances
-constraint_tol  = 1e-5
-convergence_tol = 1e-5
+constraint_tol  = 1e-4
+convergence_tol = 1e-4
 
 # time frames (a stance only)
 t0   = 0.245 # init time
