b4fc9c71779fc55b4eecad1a12a51aef8b0ee153
mrrezaie <mrrezaie@outlook.com>
2024-08-17 12:38:51 +0330

    1e-4 tol, 0.005w grf, 2000 10000w residual

--- tracking_simulation.py
+++ tracking_simulation.py
@@ -27,7 +27,7 @@ if residual <= 1:
     reduce_residuals = False
 else:
     reduce_residuals = True
-    residuals_weight = 1000 # increase the their weights in control-effort goal
+    residuals_weight = 10000 # increase the their weights in control-effort goal
 
 # solver tolerances
 constraint_tol  = 1e-4
