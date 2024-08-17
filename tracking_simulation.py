d26967fcffcc46255e164d50c3d4f4288ba6ef36
mrrezaie <mrrezaie@outlook.com>
2024-08-17 12:09:08 +0330

    1e-4 tol, 0.005w grf, 1 residual

--- tracking_simulation.py
+++ tracking_simulation.py
@@ -21,7 +21,7 @@ control_weight = 0.001 # (default==0.001 in MocoTrack)
 # actuators strength
 reserve_weak   = 1
 reserve_strong = 200 # ID<150Nm
-residual       = 2000
+residual       = 1
 
 if residual <= 1:
     reduce_residuals = False
