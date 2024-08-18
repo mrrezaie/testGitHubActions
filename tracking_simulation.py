fb471e86df1e58cfb7bd519b276626b7334be978
mrrezaie <mrrezaie@outlook.com>
2024-08-18 16:24:14 +0330

    0.0001w minimize speeds

--- tracking_simulation.py
+++ tracking_simulation.py
@@ -17,7 +17,7 @@ joint_reaction_goal = False
 marker_weight  = 1
 grf_weight     = 0.005
 control_weight = 0.001 # (default==0.001 in MocoTrack)
-speed_weight   = 0.001
+speed_weight   = 0.0001
 # PFJL_weight    = 0.1
 
 # actuators strength
