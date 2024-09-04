e9b6533c3cc08d5fc5319218fc9729d82f9b9352
Mohammadreza Rezaie <mrrezaie@outlook.com>
2024-09-04 21:33:40 +0330

    TD explicit

--- tracking_simulation.py
+++ tracking_simulation.py
@@ -373,12 +373,12 @@ print('Total number of mesh intervals', solver.get_num_mesh_intervals())
 solver.set_optim_constraint_tolerance(constraint_tol) # 0.01 MocoTrack default
 solver.set_optim_convergence_tolerance(convergence_tol) # 0.01 MocoTrack default
 solver.set_optim_max_iterations(10000)
-solver.set_minimize_implicit_multibody_accelerations(True)
-solver.set_implicit_multibody_accelerations_weight(0.0001)
+# solver.set_minimize_implicit_multibody_accelerations(True)
+# solver.set_implicit_multibody_accelerations_weight(0.0001)
 # solver.set_parameters_require_initsystem(True) # if adjust torso mass
 # solver.set_minimize_implicit_auxiliary_derivatives(True)
 # solver.set_implicit_auxiliary_derivatives_weight(0.000001)
-solver.set_multibody_dynamics_mode('implicit') # explicit implicit
+solver.set_multibody_dynamics_mode('explicit') # explicit implicit
 # solver.set_transcription_scheme('hermite-simpson') # trapezoidal
 # solver.set_interpolate_control_midpoints(True)
 # solver.set_enforce_path_constraint_midpoints(True)
